b4662e37c0e5a57123b061ff9ffe1ca1bbe9d6d8
harshita <harshita1225@gmail.com>
2022-09-05 20:44:51 +0200

    task completed

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
+class Calculator {
+  constructor() {
+    this.PI = 3.141592;
+    this.E = 2.718281828459045;
+  }
 
-class Calculator{
-    constructor(){
-        //properties
-    }
+  //getter
+  get pi() {
+    return `PI (${this.PI})`;
+  }
 
-    //getter
-    
-    //methods
+  get e() {
+    return `Eulner's number (${this.E})`;
+  }
+
+  //methods
+  ratio(x, y, width) {
+    let widthRatio = width / x;
+    let heightRatio = widthRatio * y;
+    return `height is ${heightRatio} on ratio ${x}:${y}`;
+  }
+  percentage(x, y) {
+    return (x / y) * 100;
+  }
+
+  add(x, y) {
+    return x + y;
+  }
+
+  subtract(x, y) {
+    return x - y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+
+  divide(x, y) {
+    return x / y;
+  }
+
+  modulation(x, y) {
+    return x % y;
+  }
+
+  elevate(x, y) {
+    return x ** y;
+  }
+  sqrt(x) {
+    return Math.sqrt(x);
+  }
 }
+
+const hjCalc = new Calculator();
+
+console.log(hjCalc.add(2, 4));
+console.log(hjCalc.ratio(3, 4, 600));
